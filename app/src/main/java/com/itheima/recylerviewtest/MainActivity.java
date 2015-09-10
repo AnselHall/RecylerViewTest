170c82358e86040a24e1a3ea1052a8fa26309adf
ansel <anselhall@163.com>
2015-09-10 22:24:17 +0800

    Initial commit

---
+++ app/src/main/java/com/itheima/recylerviewtest/MainActivity.java
@@ -0,0 +1,199 @@
+package com.itheima.recylerviewtest;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.main_rv)
+    RecyclerView main_rv;
+    private ArrayList<String> mDatas;
+
+    public final static String[] imageUrls = new String[]{
+            "http://img.my.csdn.net/uploads/201508/05/1438760758_3497.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760758_6667.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760757_3588.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760756_3304.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760755_6715.jpeg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760726_5120.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760726_8364.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760725_4031.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760724_9463.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760724_2371.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760707_4653.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760706_6864.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760706_9279.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760704_2341.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760704_5707.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760685_5091.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760685_4444.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760684_8827.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760683_3691.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760683_7315.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760663_7318.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760662_3454.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760662_5113.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760661_3305.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760661_7416.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760589_2946.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760589_1100.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760588_8297.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760587_2575.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760587_8906.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760550_2875.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760550_9517.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760549_7093.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760549_1352.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760548_2780.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760531_1776.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760531_1380.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760530_4944.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760530_5750.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760529_3289.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760500_7871.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760500_6063.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760499_6304.jpeg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760499_5081.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760498_7007.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760478_3128.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760478_6766.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760477_1358.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760477_3540.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760476_1240.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760446_7993.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760446_3641.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760445_3283.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760444_8623.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760444_6822.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760422_2224.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760421_2824.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760420_2660.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760420_7188.jpg",
+            "http://img.my.csdn.net/uploads/201508/05/1438760419_4123.jpg",
+    };
+
+    public static final String TAG = "REPONSE";
+    private RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        main_rv.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        //main_rv.setLayoutManager(new GridLayoutManager(this,4));
+//        main_rv.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.HORIZONTAL));
+        MyAdapter myAdapter = new MyAdapter();
+
+//        main_rv.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST));
+//        main_rv.addItemDecoration(new DividerGridItemDecoration(this));
+        main_rv.setAdapter(myAdapter);
+
+    }
+
+    public void volleytest(View view){
+        testVolley();
+    }
+    private void testVolley() {
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+
+        StringRequest stringRequest = new StringRequest("http://192.168.1.110:8080/applist1", new Response.Listener<String>() {
+            @Override
+            public void onResponse(final String response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this,response,Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        mQueue.add(stringRequest);
+        mQueue.start();
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            MyViewHolder holder = new MyViewHolder(LayoutInflater.from(MainActivity.this).inflate(R.layout.rv_item, parent, false));
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(final MyViewHolder holder, int position) {
+            queue = Volley.newRequestQueue(MainActivity.this);
+            ImageLoader imageLoader = new ImageLoader(queue, new ImageLoader.ImageCache() {
+                @Override
+                public void putBitmap(String url, Bitmap bitmap) {
+                }
+
+                @Override
+                public Bitmap getBitmap(String url) {
+                    return null;
+                }
+            });
+            holder.imageView.setDefaultImageResId(R.mipmap.ic_launcher);//默认图片，加载中显示图片
+            holder.imageView.setErrorImageResId(R.mipmap.ic_launcher);//加载失败显示图片
+
+            holder.imageView.setImageUrl(imageUrls[position],imageLoader);
+        }
+
+        @Override
+        public int getItemCount() {
+            return imageUrls.length;
+        }
+
+        public class MyViewHolder extends ViewHolder {
+
+            NetworkImageView imageView;
+            public MyViewHolder(View itemView) {
+                super(itemView);
+                imageView = ((NetworkImageView) itemView.findViewById(R.id.rv_item_tv));
+            }
+        }
+    }
+}
